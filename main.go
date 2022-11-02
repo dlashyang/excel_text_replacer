6f49e6376e0303c8c4b768f0a8035ab8e6537539
dlashyang <dlashyang@gmail.com>
2022-11-02 16:12:32 +1100

    replacing cobra with flag

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Leopold Yang
 */
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"github.com/spf13/cobra"
 	"github.com/xuri/excelize/v2"
 	"log"
 	"os"
@@ -16,73 +16,46 @@ import (
 const h1 = "#  "
 const h2 = "## "
 const block = "'''"
-
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "excel_text_replacer excel_file_name",
-	Short: "convert an excel file to text-format file",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		flagUpdate, _ := cmd.Flags().GetBool("update")
-		var excelFileName, textFileName string
-		if len(args) == 2 {
-			excelFileName = args[0]
-			textFileName = args[1]
-		} else {
-			if flagUpdate {
-				textFileName = args[0]
-				excelFileName = strings.TrimSuffix(textFileName, ".md")
-			} else {
-				excelFileName = args[0]
-				textFileName = excelFileName + ".md"
-			}
-		}
-
-		if flagUpdate {
-			fmt.Printf("updating: %s -> %s\n", textFileName, excelFileName)
-			if err := text2excel(excelFileName, textFileName); err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			fmt.Printf("coverting: %s -> %s\n", excelFileName, textFileName)
-			if err := excel2text(excelFileName, textFileName); err != nil {
-				log.Fatal(err)
-			}
-		}
-	},
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.excel_text_replacer.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("update", "u", false, "update from text file")
-}
+const helpMsg = `Usage:
+  Converting to text file: excel_text_replacer excel_file text_file
+  Updating excel file from text file: excel_text_replacer text_file excel_file`
 
 func main() {
-	execute()
+	flag.Parse()
+
+	args := flag.Args()
+	genName := false
+	if len(args) < 1 {
+		fmt.Println(helpMsg)
+		return
+	} else if len(args) == 1 {
+		genName = true
+	}
+
+	var excelFileName, textFileName string
+	if strings.HasSuffix(args[0], ".xlsx") {
+		excelFileName = args[0]
+		if genName {
+			textFileName = excelFileName + ".md"
+		} else {
+			textFileName = args[1]
+		}
+		fmt.Printf("coverting: %s -> %s\n", excelFileName, textFileName)
+		if err := excel2text(excelFileName, textFileName); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		textFileName := args[0]
+		if genName {
+			excelFileName = strings.TrimSuffix(textFileName, ".md")
+		} else {
+			excelFileName = args[1]
+		}
+		fmt.Printf("updating: %s -> new_%s\n", textFileName, excelFileName)
+		if err := text2excel(excelFileName, textFileName); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func text2excel(excelFile, textFile string) error {
